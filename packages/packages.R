815eeb97a34d6efea41768f05f70e6ee1c03e564
Khalil Belghouat <67176135+KhalilBelghouat@users.noreply.github.com>
2021-01-29 02:11:28 +0100

    Add files via upload

---
+++ packages/packages.R
@@ -0,0 +1,18 @@
+# SUPPLEMENTARY CODE FOR THE MASTER PROJECT: 
+# An Extreme Value Theory Approach to Financial Risk Modeling
+
+# This script installs and loads all R packages required to reproduce the results.
+
+# Installing R packages.
+install.packages('evir')
+install.packages('ismev')
+install.packages('extRemes')
+install.packages('numDeriv')
+install.packages('PerformanceAnalytics')
+
+# Loading the installed R packages.
+library(evir)
+library(ismev)
+library(extRemes)
+library(numDeriv)
+library(PerformanceAnalytics)
